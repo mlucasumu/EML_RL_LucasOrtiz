8277cddfbab333e126ea93e06edb9e1585edfb7c
Marta L <m.lucasmarin@um.es>
2026-02-28 15:12:26 +0100

    Añadido cálculo de probs en políticas
    En EpsilonDecay es exactamente igual que en su clase padre

--- entornos_complejos/src/policies/base_policy.py
+++ entornos_complejos/src/policies/base_policy.py
@@ -12,3 +12,14 @@ class BasePolicy(ABC):
         Dado un estado y una tabla de valores Q, devuelve la siguiente acción a realizar.
         '''
         pass
+
+    @abstractmethod
+    def action_probabilities(self, state, qtable):
+        '''
+        Calcula las probabilidades de seleccionar cada acción en el estado state.
+        '''
+        pass
+
+    def action_probability(self, state, action, qtable):
+        probs = self.action_probabilities(state, qtable)
+        return probs[action]
--- entornos_complejos/src/policies/epsilon_greedy.py
+++ entornos_complejos/src/policies/epsilon_greedy.py
@@ -9,7 +9,6 @@ class EpsilonGreedyPolicy(BasePolicy):
         self.epsilon = epsilon
 
     def select_action(self, state, qtable):
-        
         if np.random.random() < self.epsilon: # Exploración
             n_actions = len(qtable[state])
             action = np.random.choice(n_actions)
@@ -17,3 +16,10 @@ class EpsilonGreedyPolicy(BasePolicy):
             max_ids = np.where(qtable[state, :] == np.max(qtable[state, :]))[0]
             action = np.random.choice(max_ids)
         return action
+    
+    def action_probabilities(self, state, qtable):
+        n_actions = len(qtable[state])
+        probs = np.ones(n_actions) * (self.epsilon / n_actions)
+        greedy_action = np.argmax(qtable[state])
+        probs[greedy_action] += (1.0 - self.epsilon)
+        return probs
